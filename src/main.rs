aa1f975b2b67a7a51e80f1aca1d2a09f7943e5e2
ぶーと / Yoshiaki Ueda <oh@bootjp.me>
2021-02-06 18:50:10 +0900

    chapter 2

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,42 @@
+extern crate rand;
 
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
-    let n  = 22.0 / 7.0;
-    println!("Pi is rough ly  {:.3} ", n);
+    println!("guess the number");
 
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    loop {
+
+        println!("please input your guess");
+
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+
+        println!("You guessed: {}", guess);
+
+//        let guess: u32 = guess.trim().parse()
+//            .expect("Please type a number!");
+
+        let guess :u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue
+        };
+
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
 
